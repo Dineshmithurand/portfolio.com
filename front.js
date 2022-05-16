b0489612e3c7f4c006a364e970d69ae4af607ce0
Dineshmithurand <105442171+Dineshmithurand@users.noreply.github.com>
2022-05-16 08:27:55 -0600

    Add files via upload

---
+++ front.js
@@ -0,0 +1,28 @@
+// $("h1").on("click", function(){
+//         alert("The paragraph was clicked.");
+//       });
+// $("h1")
+
+
+// $("li").css({"float":"left","list-style":"none","margin-top":"20px"});
+// $("a").css({"background-color": "", 
+// "font-size": "100%",
+// "margin-left":"1px",
+// "text-decoration":"none",
+// });
+
+// $(".h").click( function(){
+//  $("p").hide()
+// });
+
+
+$("#btn").click( function(){
+  $("span01").slideToggle()
+ });
+
+//  $(".btn").click(function(){
+//    alert("cv you want open")
+//  })
+//  $("input").click(function(){
+//    alert("cv you want open")
+//  })
